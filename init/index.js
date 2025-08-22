e29642566183a9ef0165c949e23031f9a87d3111
Parth J Chaudhary <parthchaudhary.jc@yahoo.com>
2025-08-23 01:53:22 +0530

    Variables Name Corrections

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/car_rental";
+require('dotenv').config({ path: '../.env' });
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {
